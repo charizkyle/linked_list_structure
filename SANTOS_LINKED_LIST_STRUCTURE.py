c91f5a05a547240bd9fc9ed0b72eeac6adcae23d
charizkyle <santoscharizkyle@gmail.com>
2025-10-19 17:30:50 +0800

    initial commit

---
+++ SANTOS_LINKED_LIST_STRUCTURE.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
+
+    def delete_at_beginning(self):
+        if self.head:
+            removed = self.head.data
+            self.head = self.head.next
+            return removed
+        else:
+            return None
+
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next
+        print("X")
+
+
+# ➤ MAIN EXECUTION
+sushi_preparation = LinkedList()
+sushi_preparation.insert_at_end("prepare")
+sushi_preparation.insert_at_end("roll")
+sushi_preparation.insert_at_beginning("assemble")
+
+print("LINKED LIST:")
+sushi_preparation.print_list()
+
+removed = sushi_preparation.delete_at_beginning()
+print(f"\nREMOVED BEGINNING ('{removed}'):")
+sushi_preparation.print_list()
